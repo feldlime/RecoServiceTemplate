ecb26a649a5dcf8a24a95afee4e8437919f35deb
Mikhail Gurevich <gurevich3004@gmail.com>
2022-11-16 11:21:00 +0300

    Add authorization with api_key or token

--- service/api/exceptions.py
+++ service/api/exceptions.py
@@ -17,23 +17,34 @@ class AppException(Exception):
         super().__init__()
 
 
-class UserNotFoundError(AppException):
-    def __init__(
-        self,
-        status_code: int = HTTPStatus.NOT_FOUND,
-        error_key: str = "user_not_found",
-        error_message: str = "User is unknown",
-        error_loc: tp.Optional[tp.Sequence[str]] = None,
-    ):
-        super().__init__(status_code, error_key, error_message, error_loc)
-
-
 class ModelNotFoundError(AppException):
     def __init__(
         self,
         status_code: int = HTTPStatus.NOT_FOUND,
         error_key: str = "model_not_found",
         error_message: str = "Model is unknown",
+        error_loc: tp.Optional[tp.Sequence[str]] = None,
+    ):
+        super().__init__(status_code, error_key, error_message, error_loc)
+
+
+class NotAuthorizedError(AppException):
+    def __init__(
+        self,
+        status_code: int = HTTPStatus.UNAUTHORIZED,
+        error_key: str = "not_authorized",
+        error_message: str = "Authorization failed",
+        error_loc: tp.Optional[tp.Sequence[str]] = None,
+    ):
+        super().__init__(status_code, error_key, error_message, error_loc)
+
+
+class UserNotFoundError(AppException):
+    def __init__(
+        self,
+        status_code: int = HTTPStatus.NOT_FOUND,
+        error_key: str = "user_not_found",
+        error_message: str = "User is unknown",
         error_loc: tp.Optional[tp.Sequence[str]] = None,
     ):
         super().__init__(status_code, error_key, error_message, error_loc)
--- service/api/views.py
+++ service/api/views.py
@@ -1,10 +1,20 @@
+import os
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, FastAPI, Request, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.api_key import APIKey, APIKeyHeader, APIKeyQuery
 from pydantic import BaseModel
 
-from service.api.exceptions import ModelNotFoundError, UserNotFoundError
+from service.api.exceptions import (
+    ModelNotFoundError,
+    NotAuthorizedError,
+    UserNotFoundError,
+)
 from service.log import app_logger
+
+load_dotenv()
 
 
 class RecoResponse(BaseModel):
@@ -13,6 +23,27 @@ class RecoResponse(BaseModel):
 
 
 router = APIRouter()
+
+
+api_key_query = APIKeyQuery(name='API_KEY', auto_error=False)
+api_key_header = APIKeyHeader(name='API_KEY', auto_error=False)
+token_bearer = HTTPBearer(auto_error=False)
+
+API_KEY = os.getenv("API_KEY")
+
+
+async def get_api_key(
+    api_key_from_query: str = Security(api_key_query),
+    api_key_from_header: str = Security(api_key_header),
+    token: HTTPAuthorizationCredentials = Security(token_bearer),
+):
+    if api_key_from_query == API_KEY:
+        return api_key_from_query
+    if api_key_from_header == API_KEY:
+        return api_key_from_header
+    if token is not None and token.credentials == API_KEY:
+        return token.credentials
+    raise NotAuthorizedError()
 
 
 @router.get(
@@ -27,16 +58,16 @@ async def health() -> str:
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
-    responses={404: {"description": "User or model not found"}}
+    responses={404: {"description": "User or model not found"},
+               401: {"description": "Not authorized"}},
 )
 async def get_reco(
     request: Request,
     model_name: str,
     user_id: int,
+    api_key: APIKey = Depends(get_api_key)
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
-
-    # Write your code here
 
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
