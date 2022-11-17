1ef938b425a975621d986d742b629137a8cb00f9
Azat Sibagatulin <sibagatulin.azat@yandex.ru>
2022-11-17 12:18:00 +0300

    Fix: tests are up

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -20,9 +20,9 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="test_model", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": "Bearer Team_5"})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -34,8 +34,8 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="test_model", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": "Bearer Team_5"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
