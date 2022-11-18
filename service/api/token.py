3379ab55f07d78585ea7e3e54a35858b5335ffcc
Владислав Гмырь <gmyr@sputnikfund.ru>
2022-11-18 21:05:31 +0700

    Change: token type

--- service/api/token.py
+++ service/api/token.py
@@ -1,7 +1,5 @@
 import os
 
-from jose import jwt
-from jose.exceptions import JOSEError
 from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
@@ -9,12 +7,11 @@ from service.api.exceptions import NonAuthorizedError
 
 security = HTTPBearer()
 
-SECRET_KEY = os.getenv('SECRET_KEY', '7c2de62e1c23556420b0d05ea3b3543d28048ce14bfed57131ca4b9643081609')
-
+SECRET_KEY = os.getenv('SECRET_KEY', '000000')
 
 async def has_access(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-    try:
-        payload = jwt.decode(token, key=SECRET_KEY, algorithms="HS256")
-    except JOSEError as e:
+    if token == SECRET_KEY:
+        return None
+    else:
         raise NonAuthorizedError()
