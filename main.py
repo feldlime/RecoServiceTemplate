93d38483c4a55da55966a20b9af1f91471c545e3
Владислав Гмырь <gmyr@sputnikfund.ru>
2022-11-18 18:19:01 +0700

    Update: change ports in docker-compose on const

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     host = os.getenv("HOST", "127.0.0.1")
     port = int(os.getenv("PORT", "8080"))
     reload = bool(int(os.getenv('RELOAD', '0')))
-    uvicorn.run("main:app", host=host, port=port, reload=reload)
+    uvicorn.run(app, host=host, port=port, reload=reload)
