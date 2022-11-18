67ad31eafcc6274fa1a33361732582fc64881486
Владислав Гмырь <gmyr@sputnikfund.ru>
2022-11-18 19:18:10 +0700

    Add: error models to show in swagger

--- service/api/views.py
+++ service/api/views.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, Dict, List, Optional, Sequence
 
 from fastapi import APIRouter, FastAPI, Request, Path
 from pydantic import BaseModel
@@ -7,9 +7,20 @@ from service.api.exceptions import ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
 from service.api.RecModels import all_models
 
+
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
+
+
+class ErrorObject(BaseModel):
+    error_key: str
+    error_message: str
+    error_loc: Optional[Sequence[str]]
+
+
+class ErrorResponse(BaseModel):
+    errors: List[ErrorObject]
 
 
 router = APIRouter()
@@ -18,6 +29,7 @@ router = APIRouter()
 @router.get(
     path="/health",
     tags=["Health"],
+    response_model=str
 )
 async def health() -> str:
     return "I am alive"
@@ -27,18 +39,19 @@ async def health() -> str:
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
+    responses={404: {'model': ErrorResponse}}
 )
 async def get_reco(
-    request: Request,
-    model_name: str = Path(..., description='The name of testing model'),
-    user_id: int = Path(..., description='The specific id of user'),
+        request: Request,
+        model_name: str = Path(..., description='The name of testing model'),
+        user_id: int = Path(..., description='The specific id of user'),
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
     if model_name not in all_models:
         raise ModelNotFoundError(error_message=f'There is no model with name {model_name}')
 
-    if user_id > 10**9:
+    if user_id > 10 ** 9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
     k_recs = request.app.state.k_recs
