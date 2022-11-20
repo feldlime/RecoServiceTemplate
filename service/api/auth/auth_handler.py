3bfee93ad9d3ae58f759e1bf26f27517ca8accf6
Ilona Kovaleva <iloncka.ds@gmail.com>
2022-11-21 01:18:12 +0300

    ✨feature: Add bearer auth and tests

---
+++ service/api/auth/auth_handler.py
@@ -0,0 +1,32 @@
+from typing import Dict
+
+import jwt
+from decouple import config
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+def token_response(token: str):
+    return {"access_token": token}
+
+
+def signJWT(iss="MTC") -> Dict[str, str]:
+    payload = {"iss": iss, "sub": "team 4"}
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token_response(token)
+
+
+def decodeJWT(token: str) -> dict:
+
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token
+    except Exception:
+        return {}
+
+
+if __name__ == "__main__":
+
+    response = signJWT()
+    print(response["access_token"])
--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,10 +1,19 @@
 from http import HTTPStatus
 
+from decouple import config
+
+# from requests.structures import CaseInsensitiveDict
 from starlette.testclient import TestClient
 
+from service.api.auth.auth_handler import signJWT
 from service.settings import ServiceConfig
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+auth_token = signJWT()
+correct_headers = {"Authorization": f"Bearer {auth_token['access_token']}"}
 
 
 def test_health(
@@ -20,9 +29,9 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="random", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=correct_headers)
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -36,7 +45,7 @@ def test_get_reco_for_unknown_user(
     user_id = 10**10
     path = GET_RECO_PATH.format(model_name="random", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=correct_headers)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
 
@@ -44,9 +53,52 @@ def test_get_reco_for_unknown_user(
 def test_get_reco_for_unknown_model(
     client: TestClient,
 ) -> None:
+    user_id = 123
     model_name = "unknown"
-    path = GET_RECO_PATH.format(model_name=model_name, user_id=1)
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers=correct_headers)
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "model_not_found"
+
+
+def test_get_reco_unauthorized_invalid_authentication_scheme(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    model_name = "random"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    bad_headers = {"Authorization": f"X-token {auth_token}"}
+    with client:
+        response = client.get(path, headers=bad_headers)
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json()["errors"][0]["error_key"] == "http_exception"
+    assert response.json()["errors"][0]["error_message"] == "Invalid authentication credentials"
+
+
+def test_get_reco_unauthorized_invalid_token(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    model_name = "random"
+    auth_token = "wrong_auth_token"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    bad_token_headers = {"Authorization": f"Bearer {auth_token}"}
+    with client:
+        response = client.get(path, headers=bad_token_headers)
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json()["errors"][0]["error_key"] == "http_exception"
+    assert response.json()["errors"][0]["error_message"] == "Invalid token or expired token."
+
+
+def test_get_reco_unauthorized_invalid_authorization_code(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    model_name = "random"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=user_id)
+    with client:
+        response = client.get(path)
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json()["errors"][0]["error_key"] == "http_exception"
+    assert response.json()["errors"][0]["error_message"] == "Not authenticated"
