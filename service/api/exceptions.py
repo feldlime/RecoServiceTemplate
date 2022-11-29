bac44ffe95c73097e1d657cd597d84f4d1c120f4
PhPv <petrov.v.163@yandex.ru>
2022-11-29 07:42:38 +0300

    default model, 404 bad model, swagger, pep8

--- service/api/exceptions.py
+++ service/api/exceptions.py
@@ -22,7 +22,18 @@ class UserNotFoundError(AppException):
         self,
         status_code: int = HTTPStatus.NOT_FOUND,
         error_key: str = "user_not_found",
-        error_message: str = "User is unknown",
+        error_message: str = "user not found",
+        error_loc: tp.Optional[tp.Sequence[str]] = None,
+    ):
+        super().__init__(status_code, error_key, error_message, error_loc)
+
+
+class ModelNotFoundError(AppException):
+    def __init__(
+        self,
+        status_code: int = HTTPStatus.NOT_FOUND,
+        error_key: str = "model_not_found",
+        error_message: str = "module not found",
         error_loc: tp.Optional[tp.Sequence[str]] = None,
     ):
         super().__init__(status_code, error_key, error_message, error_loc)
