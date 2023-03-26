805af679d603256c61c51c765323dea26d1c416e
Nikita Borisov <nik-everyday@yandex.ru>
2023-03-27 01:44:04 +0300

    Added configuration files for service and models

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -74,7 +74,7 @@ def test_get_reco_with_unknown_model(
     with client:
         response = client.get(
             path,
-            headers={"Authorization": f"Bearer {config['token']}"},
+            headers={"Authorization": f"Bearer {config_service['token']}"},
         )
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "model_not_found"
