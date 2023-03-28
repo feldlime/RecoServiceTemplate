f1692c340f2a1f63bda252438f0edb2c1b390d58
Nikita Borisov <nik-everyday@yandex.ru>
2023-03-28 16:17:25 +0300

    Update service files

--- models/load_models.py
+++ models/load_models.py
@@ -1,11 +1,8 @@
-import dill
 import pandas as pd
 import joblib
 import yaml
 
-from rectools import Columns
 from models.ann import Ann
-from rectools.model_selection.time_split import TimeRangeSplitter
 with open('config/config_models.yml') as stream:
     config = yaml.safe_load(stream)
 
@@ -16,13 +13,13 @@ ANN = {
     'distance': None,
 }
 
-USERKNN = {
+UserKNN = {
     'model_loaded': False,
     'model': None,
     'reco_df': None,
 }
 
-POPULAR = {
+Popular = {
     'model_loaded': False,
     'reco_df': None,
 }
@@ -37,72 +34,26 @@ SVD = {
     'reco_df': None,
 }
 
-LIGHTFM = {
+LightFM = {
     'model_loaded': False,
     'reco_df': None,
 }
-interactions = None
-# do not train
-train = None
-
-
-def cv_generate():
-    global interactions
-    # train test split
-    n_folds = config['UserKnn_model_conf']['n_folds']
-    unit = config['UserKnn_model_conf']['unit']
-    n_units = config['UserKnn_model_conf']['n_units']
-    periods = config['UserKnn_model_conf']['periods']
-    freq = f"{n_units}{unit}"
-    last_date = interactions[Columns.Datetime].max().normalize()
-    start_date = last_date - pd.Timedelta(n_folds * n_units + 1, unit=unit)
-    date_range = pd.date_range(start=start_date, periods=periods, freq=freq,
-                               tz=last_date.tz)
-    # generator of folds
-    cv = TimeRangeSplitter(
-        date_range=date_range,
-        filter_already_seen=True,
-        filter_cold_items=True,
-        filter_cold_users=True,
-    )
-    return cv.split(interactions, collect_fold_stats=True).__next__()
-
-
-def load_data():
-    global train
-    global interactions
-    interactions = pd.read_csv(
-        '{0}/interactions.csv'.format(
-            config['UserKnn_model_conf']['dataset_path']
-        )
-    )
-
-    interactions.rename(columns={'last_watch_dt': Columns.Datetime,
-                                 'total_dur': Columns.Weight},
-                        inplace=True)
-    interactions['datetime'] = pd.to_datetime(interactions['datetime'])
-    (train_ids, test_ids, fold_info) = cv_generate()
-    train = interactions.loc[train_ids]
 
 
 def load_userknn():
-    global USERKNN
-    if not USERKNN['model_loaded']:
-        USERKNN['model_loaded'] = True
-        if config['UserKnn_model_conf']['online']:
-            with open(config['UserKnn_model_conf']['weight_path'], 'rb') as f:
-                USERKNN['model'] = dill.load(f)
-        else:
-            USERKNN['reco_df'] = pd.read_csv(
-                config['UserKnn_model_conf']['save_reco_df_path'],
-                encoding='utf-8',
-            )
+    global UserKNN
+    if not UserKNN['model_loaded']:
+        UserKNN['reco_df'] = pd.read_csv(
+            config['UserKnn_model_conf']['save_reco_df_path'],
+            encoding='utf-8',
+        )
+
 
 
 def load_popular():
-    global POPULAR
-    if not POPULAR['model_loaded']:
-        POPULAR['reco_df'] = pd.read_csv(
+    global Popular
+    if not Popular['model_loaded']:
+        Popular['reco_df'] = pd.read_csv(
             config['Popular_model_conf']['save_reco_df_path'],
             encoding='utf-8',
         )
@@ -127,9 +78,9 @@ def load_svd():
 
 
 def load_lightfm():
-    global LIGHTFM
-    if not LIGHTFM['model_loaded']:
-        LIGHTFM['reco_df'] = pd.read_csv(
+    global LightFM
+    if not LightFM['model_loaded']:
+        LightFM['reco_df'] = pd.read_csv(
             config['LightFM_model_conf']['save_reco_df_path'],
             encoding='utf-8',
         )
@@ -147,13 +98,11 @@ def load_ann():
         ANN['model'] = Ann(ANN['label'], ANN['distance'])
 
 
-def main():
-    global train
-    # load_data()
+if __name__ == '__main__':
     load_userknn()
     load_popular()
     load_als()
     load_svd()
     load_lightfm()
     load_ann()
-    interactions = train
+
--- service/api/views.py
+++ service/api/views.py
@@ -1,15 +1,27 @@
 from typing import List
+import yaml
 
-from fastapi import APIRouter, FastAPI, Request
+from fastapi import APIRouter, Depends, FastAPI, Request
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError
+from models.load_models import Popular, UserKNN, ALS, SVD, LightFM, ANN
+from service.api.exceptions import ModelNotFoundError, UserNotFoundError
+from service.auth.authorization import JWTBearer
 from service.log import app_logger
+
+with open('config/config_service.yml') as stream:
+    config_service = yaml.safe_load(stream)['config']
+with open('config/config_models.yml') as stream:
+    config_model = yaml.safe_load(stream)
 
 
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
+
+
+class Message(BaseModel):
+    message: str
 
 
 router = APIRouter()
@@ -18,15 +30,22 @@ router = APIRouter()
 @router.get(
     path="/health",
     tags=["Health"],
+    dependencies=[Depends(JWTBearer())],
 )
 async def health() -> str:
-    return "I am alive"
+    return "36.6"
 
 
 @router.get(
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
+    dependencies=[Depends(JWTBearer())],
+    responses={
+        200: {"response": RecoResponse},
+        403: {"response": Message},
+        404: {"response": Message},
+    },
 )
 async def get_reco(
     request: Request,
@@ -35,13 +54,54 @@ async def get_reco(
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
-    # Write your code here
-
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
-    k_recs = request.app.state.k_recs
-    reco = list(range(k_recs))
+    if model_name not in config_service['models']:
+        raise ModelNotFoundError(error_message=f"Model {model_name} not found")
+
+    if model_name == 'test_model':
+        k_recs = request.app.state.k_recs
+        reco = list(range(k_recs))
+    elif model_name == 'userknn_model':
+        userknn_reco_df = UserKNN['reco_df']
+        popular_reco_df = Popular['reco_df']
+        if user_id in userknn_reco_df.user_id.unique():
+            reco = list(userknn_reco_df[userknn_reco_df.user_id == user_id].item_id)
+        else:
+            reco = list(popular_reco_df.item_id)
+    elif model_name == 'popular_model':
+        popular_reco_df = Popular['reco_df']
+        reco = list(popular_reco_df.item_id)
+    elif model_name == 'als_model':
+        als_reco_df = ALS['reco_df']
+        popular_reco_df = Popular['reco_df']
+        if user_id in als_reco_df.user_id.unique():
+            reco = list(als_reco_df[als_reco_df.user_id == user_id].item_id)
+        else:
+            reco = list(popular_reco_df.item_id)
+    elif model_name == 'svd_model':
+        svd_reco_df = SVD['reco_df']
+        popular_reco_df = Popular['reco_df']
+        if user_id in svd_reco_df.user_id.unique():
+            reco = list(svd_reco_df[svd_reco_df.user_id == user_id].item_id)
+        else:
+            reco = list(popular_reco_df.item_id)
+    elif model_name == 'lightfm_model':
+        lightfm_reco_df = LightFM['reco_df']
+        popular_reco_df = Popular['reco_df']
+        if user_id in lightfm_reco_df.user_id.unique():
+            reco = list(lightfm_reco_df[lightfm_reco_df.user_id == user_id].item_id)
+        else:
+            reco = list(popular_reco_df.item_id)
+    elif model_name == 'ann_model':
+        model = ANN['model']
+        popular_reco_df = Popular['reco_df']
+        if user_id < len(model.label):
+            reco = list(model.predict(user_id))
+        else:
+            reco = list(popular_reco_df.item_id)
+
     return RecoResponse(user_id=user_id, items=reco)
 
 
