e7fbff7d7e61d1a351d0b8c41e90887d48c0004a
Nikita Borisov <nik-everyday@yandex.ru>
2023-04-09 16:12:23 +0300

    Added LightFM model

--- service/api/views.py
+++ service/api/views.py
@@ -1,7 +1,5 @@
 from typing import List
 
-import dill
-import pandas as pd
 import yaml
 from fastapi import APIRouter, Depends, FastAPI, Request
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
@@ -14,6 +12,7 @@ from service.api.exceptions import (
 )
 from service.api.responses import responses
 from service.log import app_logger
+from service.reco_models.model_classes import Popular, UserKNN, LightFM
 
 # config_file = 'service/config/config.yaml'
 with open('service/config/config.yaml') as stream:
@@ -28,9 +27,9 @@ class RecoResponse(BaseModel):
 router = APIRouter()
 bearer_scheme = HTTPBearer()
 
-userknn_recos_off = pd.read_csv(config['userknn_model']['offline'])
-popular_recos_off = list(pd.read_csv(
-    config['popular_model']['offline']).item_id)
+UserKNN.load_model()
+Popular.load_model()
+LightFM.load_model()
 
 
 @router.get(
@@ -56,17 +55,17 @@ async def get_reco(
     global reco
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
+    # errors
     if token.credentials != request.app.state.token:
         raise NotAuthorizedError(error_message=f"Token {user_id} is incorrect")
-
     elif model_name not in request.app.state.models:
         raise ModelNotFoundError(
             error_message=f"Model name {model_name} not found"
         )
-
     elif user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
+    # models
     k_recs = request.app.state.k_recs
     if model_name == 'test_model':
         reco = list(range(config['test_model']['n_recs']))
@@ -74,36 +73,37 @@ async def get_reco(
     elif model_name == 'userknn_model':
         # Online
         if config['userknn_model']['online_mode']:
-            with open(config['userknn_model']['online'], 'rb') as f:
-                model = dill.load(f)
-            userknn_recos = model.predict_online(user_id, k_recs)
+            userknn_model = UserKNN.model
+            reco = userknn_model.predict(user_id, k_recs)
 
-            with open(config['popular_model']['online'], 'rb') as f:
-                model = dill.load(f)
-            popular_recos = model.predict_online(user_id, k_recs)
+            if not reco:
+                reco = list(Popular.recs.item_id)
 
-            if len(userknn_recos) == k_recs:
-                reco = reco[reco.score > 3]
-            i = 0
-            reco = list(reco.item_id)
-            while len(reco) < k_recs:
-                if popular_recos[i] not in reco:
-                    reco.append(popular_recos[i])
-                i += 1
         # Offline
         else:
-            reco = userknn_recos_off[userknn_recos_off['user_id'] == user_id]
+            userknn_reco = UserKNN.recs
+            reco = userknn_reco[userknn_reco.user_id == user_id]
             if len(reco) == 0:
                 reco = []
             elif len(reco) <= k_recs:
-                reco = reco[reco.score > 2.5]
+                reco = reco[reco.score > 1.5]
                 reco = list(reco.item_id)
+
+            popular_recs = Popular.recs
+            popular_reco = list(popular_recs.item_id)
             i = 0
             while len(reco) < k_recs:
-                if popular_recos_off[i] not in reco:
-                    reco.append(popular_recos_off[i])
+                if popular_reco[i] not in reco:
+                    reco.append(popular_reco[i])
                 i += 1
 
+    elif model_name == 'lightfm_model':
+        # Online
+        lightfm_model = LightFM.model
+        reco = lightfm_model.recommend(user_id, k_recs)
+
+        if not reco:
+            reco = list(Popular.recs.item_id)
     return RecoResponse(user_id=user_id, items=reco)
 
 
---
+++ service/reco_models/model_classes.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+import dill
+import pandas as pd
+import yaml
+
+with open('service/config/config.yaml') as stream:
+    config = yaml.safe_load(stream)
+
+
+class UserKNN:
+    model_loaded: bool = False
+    model: Optional[object] = None
+    recs: Optional[pd.DataFrame] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            if config['userknn_model']['online_mode']:
+                with open(config['userknn_model']['online'], 'rb') as f:
+                    cls.model = dill.load(f)
+            else:
+                cls.recs = pd.read_csv(
+                    config['userknn_model']['offline'],
+                    encoding='utf-8',
+                )
+
+
+class Popular:
+    model_loaded: bool = False
+    model: Optional[object] = None
+    recs: Optional[pd.DataFrame] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            cls.recs = pd.read_csv(
+                config['popular_model']['offline'],
+                encoding='utf-8',
+            )
+
+
+class LightFM:
+    model_loaded: bool = False
+    model: Optional[object] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            with open(config['lightfm_model']['online'], 'rb') as f:
+                cls.model = dill.load(f)
