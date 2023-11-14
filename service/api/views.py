0179c876f89b0a3d90e3d3d6caff69d8947bd885
Leonteva Polina <polinleonteva@gmail.com>
2023-11-14 20:32:00 +0300

    fix imports sort

--- service/api/views.py
+++ service/api/views.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, FastAPI, Request
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError, ModelNotFoundError
+from service.api.exceptions import ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
 
 
