c2957e56992cd346cc751c80cbd602db7df61ea6
Tatiana Maslennikova <tanyamaslennikova24@mail.ru>
2023-11-15 16:36:37 +0300

    add first model

--- service/api/views.py
+++ service/api/views.py
@@ -1,9 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request
+from fastapi import APIRouter, FastAPI, Header, Request
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError
+from service.api.exceptions import InvalidAuthorization, ModelNotFoundError, UserNotFoundError
+from service.api.recsys.user_based_recsys import get_reccomendation
 from service.log import app_logger
 
 
@@ -15,11 +16,21 @@ class RecoResponse(BaseModel):
 router = APIRouter()
 
 
+def check_access(authorization):
+    if authorization is None:
+        raise InvalidAuthorization(error_message=f"No token")
+    token = authorization.split(" ")[-1]
+    SECRET_KEY = "mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"
+    if token != SECRET_KEY:
+        raise InvalidAuthorization(error_message=f"Invalid token {token}")
+
+
 @router.get(
     path="/health",
     tags=["Health"],
 )
-async def health() -> str:
+async def health(authorization: str = Header(None)) -> str:
+    check_access(authorization)
     return "I am alive"
 
 
@@ -28,20 +39,15 @@ async def health() -> str:
     tags=["Recommendations"],
     response_model=RecoResponse,
 )
-async def get_reco(
-    request: Request,
-    model_name: str,
-    user_id: int,
-) -> RecoResponse:
+async def get_reco(request: Request, model_name: str, user_id: int, authorization: str = Header(None)) -> RecoResponse:
+    check_access(authorization)
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
-
-    # Write your code here
-
+    if model_name != "user_based":
+        raise ModelNotFoundError(error_message=f"Model {model_name} not found")
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
-
-    k_recs = request.app.state.k_recs
-    reco = list(range(k_recs))
+    reco = get_reccomendation(user_id, request.app.state.k_recs)
+    # reco = list(range(1, 11))
     return RecoResponse(user_id=user_id, items=reco)
 
 
--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -11,7 +11,7 @@ def test_health(
     client: TestClient,
 ) -> None:
     with client:
-        response = client.get("/health")
+        response = client.get("/health", headers={"Authorization": "Bearer mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"})
     assert response.status_code == HTTPStatus.OK
 
 
@@ -19,10 +19,10 @@ def test_get_reco_success(
     client: TestClient,
     service_config: ServiceConfig,
 ) -> None:
-    user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    user_id = 864613
+    path = GET_RECO_PATH.format(model_name="user_based", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": "Bearer mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -34,8 +34,38 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name="user_based", user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, headers={"Authorization": "Bearer mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+
+def test_get_reco_with_unknown_model(
+    client: TestClient,
+) -> None:
+    model_name = "model_name"
+    path = GET_RECO_PATH.format(model_name=model_name, user_id=864613)
+    with client:
+        response = client.get(path, headers={"Authorization": "Bearer mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"})
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()["errors"][0]["error_key"] == "model_not_found"
+
+
+def test_check_authorizatin_success(
+    client: TestClient,
+) -> None:
+    model_name = "model_name"
+    with client:
+        response = client.get("/health", headers={"Authorization": "Bearer mYOHbHbOwViaarXnJGlAihcJhIjjQDUQ"})
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == "I am alive"
+
+
+def test_check_authorizatin_unsuccess(
+    client: TestClient,
+) -> None:
+    model_name = "model_name"
+    with client:
+        response = client.get("/health", headers={"Authorization": "Bearer mYOHbOwarXnJGlAihcJhIjjQDUQ"})
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
