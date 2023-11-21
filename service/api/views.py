b78d4a7f831cb2c08948d8676067deabc6c992cd
Paradocsal <super.s-vinogradov@yandex.ru>
2023-11-22 01:28:27 +0300

    lab1all

--- service/api/views.py
+++ service/api/views.py
@@ -1,18 +1,48 @@
 from typing import List
-
+from http import HTTPStatus
 from fastapi import APIRouter, FastAPI, Request
-from pydantic import BaseModel
-
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from pydantic import BaseModel, Field
+import random
 from service.api.exceptions import UserNotFoundError
 from service.log import app_logger
+from service.keysconfig import api_key
 
 
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
 
+ErrorResponseModel = BaseModel.model_construct(detail=Field(...))
+
+common_responses = {
+    200: {
+        "description": "Success!",
+        "model": RecoResponse,
+    },
+    404: {
+        "description": "Your model or user id was not found",
+        "model": BaseModel.model_construct(detail=Field(...)),
+    },
+    401: {
+        "description": "Unauthorized access",
+        "model": BaseModel.model_construct(detail=Field(...)),
+    },
+    403: {
+        "description": "Unauthorized access",
+        "model": BaseModel.model_construct(detail=Field(...)),
+    }
+}
 
 router = APIRouter()
+security = HTTPBearer()
+
+async def verify_token(http_authorization_credentials: HTTPAuthorizationCredentials = Security(security)):
+    token = http_authorization_credentials.credentials
+    if token != api_key:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Invalid token")
+
 
 
 @router.get(
@@ -27,6 +57,8 @@ async def health() -> str:
     path="/reco/{model_name}/{user_id}",
     tags=["Recommendations"],
     response_model=RecoResponse,
+    dependencies=[Security(verify_token)],
+    responses = common_responses 
 )
 async def get_reco(
     request: Request,
@@ -35,13 +67,18 @@ async def get_reco(
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
-    # Write your code here
+
+    
 
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
-
-    k_recs = request.app.state.k_recs
-    reco = list(range(k_recs))
+    if model_name != 'random':
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f"There is no model named {model_name}")
+    if model_name == "random":
+        reco = random.sample(range(1, 10), 10)
+    else:
+        k_recs = request.app.state.k_recs
+        reco = list(range(k_recs))
     return RecoResponse(user_id=user_id, items=reco)
 
 
--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
 
 from starlette.testclient import TestClient
-
+from service.keysconfig import api_key
 from service.settings import ServiceConfig
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
-
+HEADERS = {f"Your key is {api_key}"}
 
 def test_health(
     client: TestClient,
@@ -21,8 +21,8 @@ def test_get_reco_success(
 ) -> None:
     user_id = 123
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
-    with client:
-        response = client.get(path)
+    
+    response = client.get(path, headers=HEADERS)
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -35,7 +35,31 @@ def test_get_reco_for_unknown_user(
 ) -> None:
     user_id = 10**10
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
-    with client:
-        response = client.get(path)
-    assert response.status_code == HTTPStatus.NOT_FOUND
+    response = client.get(path, headers=HEADERS)
+    assert response.status_code == HTTPStatus.NOT_FOUND #404
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+def test_get_reco_invalid_model(client: TestClient) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name='dsa', user_id=user_id)
+    response = client.get(path, headers=HEADERS)
+    assert response.status_code == HTTPStatus.NOT_FOUND #404
+
+
+def test_get_reco_unauthorized(client: TestClient) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    response = client.get(path)
+    assert response.status_code == HTTPStatus.FORBIDDEN #403
+    assert response.json()["errors"][0]["error_key"] == "user_not_authorized"
+ 
+
+def test_get_reco_authorized(client: TestClient) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    response = client.get(path, headers=HEADERS)
+    assert response.status_code == HTTPStatus.OK
+    response_json = response.json()
+    assert response_json["user_id"] == user_id
+
+
