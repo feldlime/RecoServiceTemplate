c959bcb8f842297ce0c452b3a0f59ac49a1d06e6
svlpn <78540764+svlpn@users.noreply.github.com>
2023-11-28 22:58:21 +0300

    Update views.py

--- service/api/views.py
+++ service/api/views.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, FastAPI, HTTPException, Request, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from pydantic import BaseModel
 
-from recmodels.model_proc import load_model, predict
+from recmodels.model_proc import load_model
 from service.api.exceptions import UserNotFoundError
 from service.log import app_logger
 
