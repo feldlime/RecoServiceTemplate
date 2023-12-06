802a0d5d27e75895d12dd57824e5f7b56a0e6bf9
Dmitry <dru4inin.dmitry@yandex.ru>
2023-12-06 17:56:58 +0300

    fix bugs

--- service/api/my_models.py
+++ service/api/my_models.py
@@ -4,43 +4,44 @@ import pandas as pd
 
 userknn_recos = pd.DataFrame()
 unique_users_userknn = []
-PATH = "recos/userknn_recos.csv"
+PATH = "service/recos/userknn_recos.csv"
 if os.path.exists(PATH):
     userknn_recos = pd.read_csv(PATH)
     unique_users_userknn = userknn_recos["user_id"].unique()
 
 als_recos = pd.DataFrame()
 unique_users_als = []
-PATH = "recos/userknn_recos.csv"
+PATH = "service/recos/userknn_recos.csv"
 if os.path.exists(PATH):
     als_recos = pd.read_csv(PATH)
     unique_users_als = als_recos["user_id"].unique()
 
 lightfm_recos = pd.DataFrame()
 unique_users_lightfm = []
-PATH = "recos/userknn_recos.csv"
+PATH = "service/recos/userknn_recos.csv"
 if os.path.exists(PATH):
     lightfm_recos = pd.read_csv(PATH)
     unique_users_lightfm = lightfm_recos["user_id"].unique()
 
 popular_recos = pd.DataFrame()
-PATH2 = "recos/popular.csv"
+PATH2 = "service/recos/popular.csv"
 if os.path.exists(PATH2):
     popular_recos = pd.read_csv(PATH2)["item_id"].to_list()
+
+
+def popular_model():
+    return popular_recos[:10]
 
 
 def user_knn_model(user_id: int):
     if user_id in unique_users_userknn:
         user_recommendations = userknn_recos[userknn_recos["user_id"] == user_id]["item_id"].to_list()
-        if user_recommendations:
-            if len(user_recommendations) >= 10:
-                user_recommendations = user_recommendations[:10]
-            else:
-                num_popular_recos = 10 - len(user_recommendations)
-                popular_recos_subset = popular_recos[:num_popular_recos]
-                user_recommendations = user_recommendations + popular_recos_subset
+        if len(user_recommendations) >= 10:
+            user_recommendations = user_recommendations[:10]
         else:
-            user_recommendations = popular_recos[:10]
+            num_popular_recos = 10 - len(user_recommendations)
+            popular_recos_subset = popular_recos[:num_popular_recos]
+            user_recommendations = user_recommendations + popular_recos_subset
     else:
         user_recommendations = popular_recos[:10]
     return user_recommendations
@@ -49,15 +50,12 @@ def user_knn_model(user_id: int):
 def als_model(user_id: int):
     if user_id in unique_users_als:
         user_recommendations = als_recos[als_recos["user_id"] == user_id]["item_id"].to_list()
-        if user_recommendations:
-            if len(user_recommendations) >= 10:
-                user_recommendations = user_recommendations[:10]
-            else:
-                num_popular_recos = 10 - len(user_recommendations)
-                popular_recos_subset = popular_recos[:num_popular_recos]
-                user_recommendations = user_recommendations + popular_recos_subset
+        if len(user_recommendations) >= 10:
+            user_recommendations = user_recommendations[:10]
         else:
-            user_recommendations = popular_recos[:10]
+            num_popular_recos = 10 - len(user_recommendations)
+            popular_recos_subset = popular_recos[:num_popular_recos]
+            user_recommendations = user_recommendations + popular_recos_subset
     else:
         user_recommendations = popular_recos[:10]
     return user_recommendations
@@ -66,15 +64,12 @@ def als_model(user_id: int):
 def lightfm_model(user_id: int):
     if user_id in unique_users_lightfm:
         user_recommendations = lightfm_recos[lightfm_recos["user_id"] == user_id]["item_id"].to_list()
-        if user_recommendations:
-            if len(user_recommendations) >= 10:
-                user_recommendations = user_recommendations[:10]
-            else:
-                num_popular_recos = 10 - len(user_recommendations)
-                popular_recos_subset = popular_recos[:num_popular_recos]
-                user_recommendations = user_recommendations + popular_recos_subset
+        if len(user_recommendations) >= 10:
+            user_recommendations = user_recommendations[:10]
         else:
-            user_recommendations = popular_recos[:10]
+            num_popular_recos = 10 - len(user_recommendations)
+            popular_recos_subset = popular_recos[:num_popular_recos]
+            user_recommendations = user_recommendations + popular_recos_subset
     else:
         user_recommendations = popular_recos[:10]
     return user_recommendations
